c6b25b31ca9fca5d2f7eb96f0c08fdb87e1197ae
Freek de Haas <freekdh@gmail.com>
2018-04-11 13:35:36 -0700

    Working version, going to change DataSet class to vector of data blocks

--- Main.cpp
+++ Main.cpp
@@ -7,9 +7,7 @@
 #include <stdlib.h>
 #include <ctime>
 #include <iomanip>
-
-/// make genome circular
-// Work on viability selection
+#include <chrono>
 
 boost::dynamic_bitset<> r_global;
 boost::dynamic_bitset<> m_global;
@@ -19,13 +17,15 @@ struct Parameters
     //default parameters
     double RECOMBINATIONRATE;
     double MUTATIONRATE;
-    double FERTILITY;
+    double INTRINSIC_GROWTHRATE;
     int NGEN;
     int NLOCI;
     int NPLOIDY;
     int NREP;
     int NINIT[2];
+    int K;
     std::vector<double> SC_GENOME;
+    std::vector<int> index;    
     std::vector<int> v;
 
     std::vector<boost::dynamic_bitset<>> INIT_GENOME[2];
@@ -34,7 +34,6 @@ struct Parameters
     double SC_LOCAL;
     int NLOCAL_ADAPTED_LOCI;
 };
-
 
 class Individual
 {
@@ -87,25 +86,18 @@ class Individual
         }
     }
 
-    double MultiplicativeViability(const std::vector<double> &scgenome)
+    double MultiplicativeViability(const Parameters &pars)
     {
-        const int NPLOIDY = genome.size();
-        const int NLOCI = genome[0].size();
-        assert(scgenome.size() == genome[0].size());
-        double output = 1.0;
-        for (int i = 0; i < NPLOIDY; ++i)
-        {
-            for (int j = 0; j < NLOCI; ++j)
-            {
-                if (genome[i][j] == 1)
-                {
-                    output *= (1.0 - scgenome[j]);
-                }
+        // Sally? what is going on here. Don't understand this multiplicative viability thing.
+        double viability = 1.0;
+        for(int i = 0; i < pars.NPLOIDY; ++i){
+            for(int j = 0; j < pars.NLOCAL_ADAPTED_LOCI+1; ++j){
+                if(genome[i][pars.index[j]] == true){viability *= pars.SC_GENOME[pars.index[j]];}
             }
         }
-        output /= (double)NLOCI;
-        assert(0.0 <= output <= 1.0);
-        return output;
+
+        assert(0.0 <= viability <= 1.0);
+        return viability;
     }
 
     void Flipbit(const int &chromosome, const int &locus) { genome[chromosome][locus].flip(); }
@@ -118,8 +110,8 @@ class Individual
         // v[Nlocal+1 : v.end()] = location of neutral loci
         int counter = 0;
         counter += genome[chromosome].count();
-        for(int i = 0; i < pars.NLOCAL_ADAPTED_LOCI; ++i){
-            counter -= genome[chromosome][pars.v[i+1]];
+        for(int i = 0; i < pars.NLOCAL_ADAPTED_LOCI + 1; ++i){
+            counter -= genome[chromosome][pars.index[i]];
         }
         assert(counter>=0);
         return counter;
@@ -153,29 +145,50 @@ struct DataSet
         data.resize(pars->NPLOIDY, std::vector<int>(pars->NLOCI, 0));
         NeutralCount[0].resize(pars->NGEN,0);
         NeutralCount[1].resize(pars->NGEN,0);
+        TotalPopulationSizeData[0].resize(pars->NGEN,0);
+        TotalPopulationSizeData[1].resize(pars->NGEN,0);
     }
 
-    void AddCount(std::vector<int> data[2]) {
-       assert(data[0].size() == data[1].size());
-       assert(data[0].size() == NeutralCount[0].size());
-       for(int i = 0; i < pars->NGEN; ++i){
-           NeutralCount[0][i] += data[0][i];
-           NeutralCount[1][i] += data[1][i];
+    void AddCount(const std::vector<int> data[2], const std::vector<int> popsize[2]) {
+        assert(data[0].size() == data[1].size());
+        assert(data[0].size() == NeutralCount[0].size());
+        for(int i = 0; i < pars->NGEN; ++i){
+            NeutralCount[0][i] += data[0][i];
+            NeutralCount[1][i] += data[1][i];
+            TotalPopulationSizeData[0][i] += popsize[0][i];
+            TotalPopulationSizeData[1][i] += popsize[1][i];
        }
     }
 
+    void AddSC_GENOME(const std::vector<double> SC_GENOME){
+        SC_GENOME_data.push_back(SC_GENOME);
+    }
+
     void WriteOutput(std::ofstream &output){
-        output << "Generation" << output.fill() << "NeutralCount0" << output.fill() << "NeutralCount1" << std::endl; 
+        output 
+        << "Generation" << output.fill() 
+        << "NeutralCount0" << output.fill() 
+        << "NeutralCount1" << output.fill() 
+        << "PopSize0" << output.fill() 
+        << "PopSize1" << output.fill() << std::endl; 
         for(int i = 0; i < pars->NGEN; ++i){
-            output << i << output.fill() << NeutralCount[0][i] << output.fill() << NeutralCount[1][i] << std::endl;
+            output
+            << i << output.fill() 
+            << NeutralCount[0][i] / ((double)pars->NREP * (double)pars->NPLOIDY * (double)(pars->NLOCI - pars->NLOCAL_ADAPTED_LOCI-1)) << output.fill() 
+            << NeutralCount[1][i] / ((double)pars->NREP * (double)pars->NPLOIDY * (double)(pars->NLOCI - pars->NLOCAL_ADAPTED_LOCI-1)) << output.fill()
+            << (double)TotalPopulationSizeData[0][i] / (double)(pars->NREP) << output.fill()
+            << (double)TotalPopulationSizeData[1][i] / (double)(pars->NREP) << output.fill() << std::endl;
         }
 
         output << "\n \n \n \n" << std::endl;
     }
 
+    std::vector<std::vector<double>> SC_GENOME_data;
+
     private:
     std::vector<std::vector<int>> data; // data[NGEN][NLOCI]
     std::vector<int> NeutralCount[2];
+    std::vector<int> TotalPopulationSizeData[2];
     const Parameters *pars;
     int counter = 0;
 };
@@ -206,9 +219,11 @@ void ResetMGlobal(boost::dynamic_bitset<> &global, const int &GLOBALMAX, const d
 
 void ItteratePopulation(std::vector<Individual*> &population, const Parameters &pars)
 {
-    // Constant fertility 
+    // Density dependent birth rate (fertility)
     const int popsize = population.size();
-    std::vector<Individual*> offspring(rnd::poisson((double)popsize * pars.FERTILITY));
+    std::vector<Individual*> offspring(rnd::poisson(
+        (double)popsize * (1.0 + pars.INTRINSIC_GROWTHRATE * (1.0 - ((double)popsize / (double)pars.K)))
+        ));
 
     // Mating + Create offspring
     for (it = offspring.begin(); it != offspring.end(); ++it)
@@ -227,7 +242,7 @@ void ItteratePopulation(std::vector<Individual*> &population, const Parameters &
     // Viability selection on offspring
     for (it = offspring.begin(); it != offspring.end(); ++it)
     {
-        if (rnd::uniform() < (*it)->MultiplicativeViability(pars.SC_GENOME))
+        if (rnd::uniform() < (*it)->MultiplicativeViability(pars))
         {
             population.push_back(*it);
         }
@@ -253,17 +268,10 @@ std::string ReturnTimeStamp(const std::string &CurrentDirectory)
     return out;
 }
 
-void OutputParameters(std::ofstream &ofstream, const Parameters &pars)
+void OutputParameters(std::ofstream &ofstream, const Parameters &pars, const DataSet &data)
 {
 
     ofstream.fill(',');
-
-    ofstream << "SC_GENOME" << ofstream.fill();
-    for (int i = 0; i < pars.NLOCI; ++i)
-    {
-        ofstream << pars.SC_GENOME[i] << ofstream.fill();
-    }
-    ofstream << std::endl;
 
     ofstream << "SC_MAJOR" << ofstream.fill() << pars.SC_MAJOR << std::endl;
     ofstream << "SC_LOCAL" << ofstream.fill() << pars.SC_LOCAL << std::endl;
@@ -279,7 +287,17 @@ void OutputParameters(std::ofstream &ofstream, const Parameters &pars)
     ofstream << "NREP" << ofstream.fill() << pars.NREP << std::endl;
     ofstream << "MUTATIONRATE" << ofstream.fill() << pars.MUTATIONRATE << std::endl;
     ofstream << "RECOMBINATIONRATE" << ofstream.fill() << pars.RECOMBINATIONRATE << std::endl;
-    ofstream << "FERTILITY" << ofstream.fill() << pars.FERTILITY << std::endl;
+    ofstream << "INTRINSICGROWTHRATE" << ofstream.fill() << pars.INTRINSIC_GROWTHRATE << std::endl;
+    ofstream << "CARRYINGCAPACITY" << ofstream.fill() << pars.K << std::endl;
+
+    ofstream << "SC_GENOME" << std::endl;
+    for (int i = 0; i < data.SC_GENOME_data.size(); ++i){
+        for (int j = 0; j < pars.NLOCI; ++j) {
+            ofstream << data.SC_GENOME_data[i][j] << ofstream.fill();
+        }
+        ofstream << std::endl;
+    }
+    ofstream << std::endl;
 }
 
 std::string CreateOutputStreams(std::ofstream &ParametersOfstream, std::ofstream &DataOfstream)
@@ -304,54 +322,63 @@ std::string CreateOutputStreams(std::ofstream &ParametersOfstream, std::ofstream
     return MainOutputFolder;
 }
 
-void ReturnRescue(std::vector<Individual*> population, std::vector<int> P[2], const int &gen, const Parameters &pars){
+void ReturnStats(std::vector<Individual*> population, std::vector<int> P[2], std::vector<int> A[2], const int &gen, const Parameters &pars){
     for(it = population.begin(); it != population.end(); ++it){
         for(int i = 0; i < pars.NPLOIDY; ++i){
-            P[(*it)->Genotype(i,pars.v[0])][gen] += (*it)->Neutralloci(i,pars);
+            P[(*it)->Genotype(i,pars.index[0])][gen] += (*it)->Neutralloci(i,pars);
+            ++A[(*it)->Genotype(i,pars.index[0])][gen];
         }
     }
 }
 
-void RunSimulation(Parameters &pars, DataSet &data)
+void RunSimulation(const Parameters &GlobalPars, DataSet &data)
 {
-    // Decide on major rescue locus (element 0) and element 1:n are locally favored loci
-    pars.v.clear();
-    pars.v.resize(pars.NLOCI);
-    std::iota(std::begin(pars.v), std::end(pars.v), 0);
-    assert(pars.v[0]==0); assert(pars.v[1]==1);
-    std::random_shuffle(pars.v.begin(), pars.v.end());
+    // Set local parameters
+    Parameters SimPars = GlobalPars;
 
-    // Set SC_GENOME for this simulation
-    pars.SC_GENOME.clear();
-    pars.SC_GENOME.resize(pars.NLOCI, 0.0);
-    pars.SC_GENOME[pars.v[0]] = pars.SC_MAJOR;
-    for (int i = 0; i < pars.NLOCAL_ADAPTED_LOCI; ++i)
+    SimPars.index.clear();
+    SimPars.index.resize(SimPars.NLOCI);
+    std::iota(std::begin(SimPars.index), std::end(SimPars.index), 0);
+    std::random_shuffle(SimPars.index.begin(), SimPars.index.end());
+
+    SimPars.SC_GENOME.clear();
+    SimPars.SC_GENOME.resize(SimPars.NLOCI, 0.0);
+    SimPars.SC_GENOME[SimPars.index[0]] = SimPars.SC_MAJOR;          // Major allele is index = 0;
+    for (int i = 0; i < SimPars.NLOCAL_ADAPTED_LOCI; ++i)
     {
-        pars.SC_GENOME[pars.v[i + 1]] = pars.SC_LOCAL;
+        SimPars.SC_GENOME[SimPars.index[i + 1]] = SimPars.SC_LOCAL;  // Local adapted alleles index 1 - nlocaladaptedloci;
     }
 
-    // Collect data
-    std::vector<int> P[2];
-    P[0].resize(pars.NGEN,0);
-    P[1].resize(pars.NGEN,0);
+    data.AddSC_GENOME(SimPars.SC_GENOME);
+
+    std::vector<int> P[2], A[2];
+    std::vector<int> TotalPopSize(SimPars.NGEN,0);
+    
+    P[0].resize(SimPars.NGEN,0); //present
+    A[0].resize(SimPars.NGEN,0);
+    P[1].resize(SimPars.NGEN,0); //present
+    A[1].resize(SimPars.NGEN,0);
 
     // Initialize population
-    std::vector<Individual*> population(pars.NINIT[0] + pars.NINIT[1]);
-    for (it = population.begin(); it != population.begin() + pars.NINIT[0]; ++it)
+    std::vector<Individual*> population(SimPars.NINIT[0] + SimPars.NINIT[1]);
+    for (it = population.begin(); it != population.begin() + SimPars.NINIT[0]; ++it)
     {
-        *it = new Individual(pars.INIT_GENOME[0]);
+        *it = new Individual(SimPars.INIT_GENOME[0]);
     }
-    for (it = population.begin() + pars.NINIT[0]; it != population.end(); ++it)
+    for (it = population.begin() + SimPars.NINIT[0]; it != population.end(); ++it)
     {
-        *it = new Individual(pars.INIT_GENOME[1]);
-    }
-
-    for (int i = 0; i < pars.NGEN; ++i)
-    {
-        ItteratePopulation(population, pars);
+        *it = new Individual(SimPars.INIT_GENOME[1]);
     }
 
-    data.AddCount(P);
+    for (int i = 0; i < SimPars.NGEN; ++i)
+    {
+        ReturnStats(population,P,A,i,SimPars);
+        TotalPopSize[i] = population.size();
+        ItteratePopulation(population, SimPars);
+    }
+
+    data.AddCount(P,A);
+    
 
     for (it = population.begin(); it != population.end(); ++it)
     {
@@ -363,7 +390,8 @@ void RunSimulation(Parameters &pars, DataSet &data)
 void AssertParameters(Parameters &pars){
     // Make sure all parameters are there
     assert(pars.NREP > 0);
-    assert(pars.NLOCI > 0);
+    assert(pars.NLOCI > 1);
+    assert(pars.NLOCI >= pars.NLOCAL_ADAPTED_LOCI + 1);// nlocaladaptedloci + major locus
     assert(pars.NPLOIDY % 2 == 0 && pars.NPLOIDY > 0);
     assert(pars.NINIT[0] > 0);
     assert(pars.NINIT[1] > 0);
@@ -376,54 +404,56 @@ void AssertParameters(Parameters &pars){
 
 int main(int argc, char *argv[])
 {
-    if (argc != 10) {std::cout << "Argc != 10" << std::endl; return -1;}
-
-    const int GLOBALMAX = 100000;
+    rnd::set_seed();
+    srand(static_cast<unsigned>(std::chrono::high_resolution_clock::now().time_since_epoch().count()));
 
     // Parameters
-    Parameters pars;
-    pars.RECOMBINATIONRATE = 0.5;
-    pars.MUTATIONRATE = 0.00001;
-    pars.FERTILITY = 1.1;
+    if (argc != 12) {std::cout << "Argc != 10" << std::endl; return -1;}
+    const int GLOBALMAX = 100000;
+    Parameters GlobalPars;
+    {
+        GlobalPars.RECOMBINATIONRATE = atof(argv[10]);
+        GlobalPars.MUTATIONRATE = 0.0;
+        GlobalPars.INTRINSIC_GROWTHRATE = 0.1;
+        GlobalPars.K = atoi(argv[11]);
 
-    pars.NLOCI = atoi(argv[1]);
-    pars.NPLOIDY = atoi(argv[2]);
-    pars.NINIT[0] = atoi(argv[3]);
-    pars.NINIT[1] = atoi(argv[4]);
-    pars.NLOCAL_ADAPTED_LOCI = atoi(argv[5]);
-    pars.SC_MAJOR = atof(argv[6]);
-    pars.SC_LOCAL = atof(argv[7]);
-    pars.NGEN = atoi(argv[8]);
-    pars.NREP = atoi(argv[9]);
+        GlobalPars.NLOCI = atoi(argv[1]);
+        GlobalPars.NPLOIDY = atoi(argv[2]);
+        GlobalPars.NINIT[0] = atoi(argv[3]);
+        GlobalPars.NINIT[1] = atoi(argv[4]);
+        GlobalPars.NLOCAL_ADAPTED_LOCI = atoi(argv[5]);
+        GlobalPars.SC_MAJOR = atof(argv[6]);
+        GlobalPars.SC_LOCAL = atof(argv[7]);
+        GlobalPars.NGEN = atoi(argv[8]);
+        GlobalPars.NREP = atoi(argv[9]);
 
-    pars.SC_GENOME.clear();
-    pars.SC_GENOME.resize(pars.NLOCI, 0.0);
-    std::vector<boost::dynamic_bitset<>> INIT_GENOME0(pars.NPLOIDY, boost::dynamic_bitset<>(pars.NLOCI, false));
-    std::vector<boost::dynamic_bitset<>> INIT_GENOME1(pars.NPLOIDY, boost::dynamic_bitset<>(pars.NLOCI, true));
-    pars.INIT_GENOME[0] = INIT_GENOME0;
-    pars.INIT_GENOME[1] = INIT_GENOME1;
+        GlobalPars.SC_GENOME.clear();
+        GlobalPars.SC_GENOME.resize(GlobalPars.NLOCI, 0.0);
+        std::vector<boost::dynamic_bitset<>> INIT_GENOME0(GlobalPars.NPLOIDY, boost::dynamic_bitset<>(GlobalPars.NLOCI));
+        std::vector<boost::dynamic_bitset<>> INIT_GENOME1(GlobalPars.NPLOIDY, boost::dynamic_bitset<>(GlobalPars.NLOCI).set());
 
-    AssertParameters(pars);
+        GlobalPars.INIT_GENOME[0] = INIT_GENOME0;
+        GlobalPars.INIT_GENOME[1] = INIT_GENOME1;
+    }
+    AssertParameters(GlobalPars);
+    ResetRGlobal(r_global, GLOBALMAX, GlobalPars.RECOMBINATIONRATE);
+    ResetMGlobal(m_global, GLOBALMAX, GlobalPars.MUTATIONRATE);
 
-    // Initialize global variables
-    rnd::set_seed();
-    ResetRGlobal(r_global, GLOBALMAX, pars.RECOMBINATIONRATE);
-    ResetMGlobal(m_global, GLOBALMAX, pars.MUTATIONRATE);
 
-    DataSet data(&pars);
-    for (int i = 0; i < pars.NREP; ++i)
+    // Simulations
+    DataSet data(&GlobalPars);
+    for (int i = 0; i < GlobalPars.NREP; ++i)
     {
         std::cout << "Replicate: " << i << std::endl;
-        RunSimulation(pars, data); // input, output
+        RunSimulation(GlobalPars, data); // input, output
     }
 
-    // DATA OFFSTREAM
+    // Output
     std::ofstream Parametersoff, Dataoff;
     std::string MainFolder;
     MainFolder = CreateOutputStreams(Parametersoff,Dataoff); 
 
-
-    OutputParameters(Parametersoff,pars);
+    OutputParameters(Parametersoff,GlobalPars, data);
 	data.WriteOutput(Dataoff);
 
     return 0;

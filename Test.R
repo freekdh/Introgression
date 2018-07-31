f417bd1988e168a559b3b49bef15cfa286c6439f
Freek de Haas <freek@debian.telus>
2018-07-31 18:13:45 -0400

    remove .o files

--- Test.R
+++ Test.R
@@ -8,8 +8,8 @@ testpars <- list(
     distlocal=1,
     scmajor=0.0,
     sclocal=0.0,
-    ngen=20,
-    nrep=50,
+    ngen=100,
+    nrep=1000,
     rec=0.5,
     k=100)
 
@@ -17,8 +17,10 @@ ShinyInitializeSimulation(testpars)
 ShinyRunSimulation()
 ShinyWriteOutputandCleanup()
 
-hello <- RcppIntrogressionSimulation(testpars,0,1)
+hello <- RcppIntrogressionSimulation(testpars,4)
 
+names(hello)
+plot(hello$data$Popsize_avg)
 ## shiny stuff
 
 shiny::runApp("/home/freek/Documents/VisualCode/C++/Introgression/pkgIntrogression/ShinyApp", display.mode = "normal")

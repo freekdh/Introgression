4b22709629fa63dc3f705f3f9e090f8d64ee8837
Freek de Haas <freek@debian.telus>
2018-07-31 16:36:02 -0400

    ShinyApp hopefully works

--- pkgIntrogression/R/ShinyExport.R
+++ pkgIntrogression/R/ShinyExport.R
@@ -1,11 +1,6 @@
 
 runExample <- function() {
-    require(shiny)
-    shinyApp(ui = "ui.r", server = "server.r")
-}
-
-runExample <- function() {
-  appDir <- system.file("shiny-examples", "myapp", package = "mypackage")
+  appDir <- system.file("ShinyApp", package = "pkgIntrogression")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
   }

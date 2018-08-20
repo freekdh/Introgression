85a915da19d6b1f4300fd039c06b8b1d1c03275d
Freek de Haas <freek@debian.telus>
2018-08-20 19:16:54 -0400

    Replace V2 with normal version

--- Rfiles/TestPackage.R
+++ Rfiles/TestPackage.R
@@ -5,22 +5,22 @@ testpars <- list(
     da=0.8,
     r=0.5,
     nloci=100,
-    ninit0=10,
+    ninit0=20,
     ninit1=2,
     ngen=20,
     nrep=1000,
     rec=0.5,
-    k=100)
+    k=20)
 
 ShinyInitializeSimulation(testpars)
 ShinyRunSimulation()
 ShinyWriteOutputandCleanup()
 
-hello <- RcppIntrogressionSimulation(testpars,0)
-names(hello)
-hello$pars
+testdata <- RcppIntrogressionSimulation(testpars,0)
 
-plot(hello$data$Popsize_avg)
+plot(testdata$data$Major0_avg)
+plot(testdata$data$Major1_avg)
+plot(testdata$data$Popsize_avg)
 ## shiny stuff
 
 shiny::runApp("/home/freek/Documents/VisualCode/C++/Introgression/pkgIntrogression/inst/ShinyApp", display.mode = "normal")
